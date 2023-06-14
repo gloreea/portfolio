1099d76a91e90533541ec3df50f70484b16c131c
gloreea <aranda_gloree408@icloud.com>
2023-06-14 15:01:04 -0700

    navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,8 +4,11 @@ export default function Navbar() {
     return (
         <nav>
             <Link href="/">Home</Link>
-            
+
             <Link href="/about">About Me</Link>
+            <Link href="/skills">Skills</Link>
+            <Link href="/projects">Projects</Link>
+            <Link href="/contact">Contact</Link>
         </nav>
     )
 }
